caaa1e178d839d8baaa879633602aa75a1f898a1
sam-kash <kashyapsankalpa@gmail.com>
2025-12-24 21:14:21 +0530

    Backend running initially

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,7 @@
+export const register = async (req, res) => {
+  res.json({ message: "Register API working" });
+};
+
+export const login = async (req, res) => {
+  res.json({ message: "Login API working" });
+};
